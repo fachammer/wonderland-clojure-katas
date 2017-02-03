662dcf081246893965c0995c30819876bf9ef9f0
Fabian Achammer <fabian.achammer@students.fh-hagenberg.at>
2017-02-03 17:05:18 +0100

    Solution with decipher

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,48 @@
 (ns alphabet-cipher.coder)
 
+(def alphabet (vec "abcdefghijklmnopqrstuvwxyz"))
+
+(def indexed-alphabet (zipmap alphabet (iterate inc 0)))
+
+(defn char->index [c]
+  (indexed-alphabet c))
+
+(defn index->char [i]
+  (alphabet i))
+
+(defn encode-char [c secret]
+  (let [col (char->index secret)
+        row (char->index c)
+        encoded-index (mod (+ row col) (count alphabet))]
+    (index->char encoded-index)))
+
 (defn encode [keyword message]
-  "encodeme")
+  (apply str (map encode-char message (cycle keyword))))
+
+(defn decode-char [c secret]
+  (let [col (char->index secret)
+        row (char->index c)
+        decoded-index (mod (+ row (- (count alphabet) col)) (count alphabet))]
+    (index->char decoded-index)))
 
 (defn decode [keyword message]
-  "decodeme")
+  (apply str (map decode-char message (cycle keyword))))
+
+(defn secret-char [original cipher]
+  (index->char (mod (- (char->index cipher) (char->index original)) (count alphabet))))
+
+(defn full-secret [original cipher]
+  (map secret-char original cipher))
+
+(defn encodes? [secret original cipher]
+  (= (encode secret original) cipher))
+
+(defn pyramid [tail]
+  (rest (reductions conj [] tail)))
+
+(defn secret-candidates [secret message cipher]
+  (filter #(encodes? (apply str %) message cipher) (pyramid secret)))
 
 (defn decipher [cipher message]
-  "decypherme")
-
+  (let [secret (full-secret message cipher)]
+    (->> (secret-candidates secret message cipher) first (apply str))))
